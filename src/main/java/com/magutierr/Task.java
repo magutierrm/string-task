f5d429fd06c0cd128ff0b67232ec61eb16cb05de
Manuel Gutierrez <mguti@softserveinc.com>
2024-02-06 12:46:23 -0500

    Solution for the given task

---
+++ src/main/java/com/magutierr/Task.java
@@ -0,0 +1,27 @@
+package com.magutierr;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Task {
+
+    /**
+     * Removes duplicate characters from a string using a LinkedHashSet.
+     * Sets are data structures that do not accept duplicate values. The LinkedHashSet
+     * implementation should be used to preserve the order of the given string.
+     *
+     * @param string The string from which duplicate characters are to be removed.
+     * @return A string without duplicate characters.
+     */
+
+    public static String removeDuplicates(String string) {
+        Set<Character> hashSet = new LinkedHashSet<>();
+        StringBuilder withoutDuplicates = new StringBuilder();
+        for (Character character : string.toCharArray()) {
+            hashSet.add(character);
+        }
+        hashSet.forEach(withoutDuplicates::append);
+        return withoutDuplicates.toString();
+    }
+
+}
---
+++ src/test/java/com/magutierr/RemoveDuplicateTests.java
@@ -0,0 +1,41 @@
+package com.magutierr;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+public class RemoveDuplicateTests {
+
+    @Test
+    public void givenStrings_thenRemoveAnyDuplicate() {
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("     "), " ");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("AABBCCD112233"), "ABCD123");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("AAAABBBCCC"), "ABC");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("123455667788"), "12345678");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("abcdefg"), "abcdefg");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("AAABBBCCCDDDEEE"), "ABCDE");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates(""), "");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("111222333444"), "1234");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("AAA"), "A");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("ABABAB"), "AB");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("A1B2C3D4"), "A1B2C3D4");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("ABCDabcd"), "ABCDabcd");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("1122334455"), "12345");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("aaaaabbb"), "ab");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("ABABABAB"), "AB");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("AAA111BBB222"), "A1B2");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("!@#$%^&*()_+"), "!@#$%^&*()_+");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates(":))))))"), ":)");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates(":("), ":(");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("aAbBcC"), "aAbBcC");
+        givenStrings_thenCheckForEquality(Task.removeDuplicates("2++2?"), "2+?");
+    }
+
+    @Test
+    public void givenStrings_thenCheckForEquality(String actual, String expected) {
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}
